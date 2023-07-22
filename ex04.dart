976f754e9ce34c959d9cb0ca02bdc0b3e78440db
Yuki <109759477+ikyu-gnito@users.noreply.github.com>
2023-07-22 00:00:45 -0300

    ex 4 e 5

--- ex04.dart
+++ ex04.dart
@@ -1,5 +1,40 @@
 /*
 Crie um programa para uma Locadora de jogos, que deverá conter a classe Jogo(Gênero e nome) e dizer se foi alugado ou não.
 */
+import 'dart:io';
 
+class Jogo {
+  late String nome, genero;
+  int estoque = 1;
 
+  Jogo(this.nome, this.genero);
+  void alugar() {
+    this.estoque--;
+  }
+}
+
+void main() 
+{
+  Jogo jogo = Jogo("Dark Souls", "RPG");
+  Jogo jogo1 = Jogo("Battefield", "FPS");
+
+  if (jogo.estoque <= 0)
+  {
+    print("ja era, o ${jogo.nome} já foi alugado");
+  } 
+  else 
+  {
+    print("o ${jogo.nome} ainda não foi alugado, voce quer?");
+  }
+
+  jogo1.alugar();
+
+  if (jogo1.estoque <= 0) 
+  {
+    print("ora ora, o ${jogo1.nome} ja foi alugado little friend");
+  } 
+  else 
+  {
+    print("o ${jogo1.nome} ainda está disponivel, mas pra voce nao");
+  }
+}
--- ex05.dart
+++ ex05.dart
@@ -1,5 +1,36 @@
 /*
 Criar uma estrutura hierárquica que contenha as seguintes classes: Veiculo (classe abstrata), Bicicleta e Automóvel.
 */
+import 'dart:io';
+
+abstract class Veiculo {
+  late String fabricante;
+  Veiculo(this.fabricante);
+}
+
+class Bicicleta extends Veiculo {
+  late String marca;
+  Bicicleta(this.marca, String fabricante) : super(fabricante);
+}
+
+class Automovel extends Veiculo {
+  late String marca, veiculo;
+  Automovel(this.marca, this.veiculo, String fabricante) : super(fabricante);
+}
+
+void main() {
+  String nome;
+  Bicicleta bicicleta = Bicicleta("caloi", "caloi");
+  Automovel auto = Automovel("Yamaha", "moto", "Yamaha");
+  print("voce vai andar com o que?");
+  nome = stdin.readLineSync()!;
+  if (nome == auto.veiculo) {
+    print("voce vai de moto");
+  } else if (nome == "bicicleta") {
+    print("voce vai de bicicleta");
+  } else {
+    print("a opção não existe, vai a pé.");
+  }
+}
 
 
